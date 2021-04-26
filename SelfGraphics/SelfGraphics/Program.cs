fc3b20aff198e218e1f5fe4419005941510c263e
qwertyuio <mmm606@inbox.ru>
2021-04-26 17:13:03 +0300

    Name update

--- SelfGraphics/SelfGraphics/Program.cs
+++ SelfGraphics/SelfGraphics/Program.cs
@@ -14,37 +14,40 @@ using System.Threading;
 
 namespace SelfGraphics
 {
-
     class Program
     {
-        static RenderWindow window;
+        static RenderWindow _window;
 
-        public const int rays = 100;
+        public const int RAYS = 75;
 
-        public static double angle;
+        public static double _angle;
+
+        const double Height = 900;
+
+        const double Wight = 1500;
 
         public static double CamAngel
         {
-            
-            get => angle;
+            get => _angle;
             set
             {
-              
-                angle = value;
-                cam.SetRoration(angle);
-                cam.Render(rays, MaxLen);
+                _angle = value;
+                cam.SetRoration(_angle);
+                cam.Render(RAYS, MaxLen);
             }
         }
+
         public static double GetEven(double f)
         {
             f = Math.Round(f);
             if (f % 2 == 0) return f + 1;
             return f;
         }
+
         public static void DrawRender()
         {
             RenderWindow render = new RenderWindow(new VideoMode(750, 200), "Render");
-            render.Closed += (o, args) => { window.Close(); };
+            render.Closed += (o, args) => { _window.Close(); };
             render.SetActive(true);
             while (render.IsOpen)
             {
@@ -56,61 +59,65 @@ namespace SelfGraphics
 
 
         public const double MaxLen = 1000;
+
         public static double PerspectiveHeight(double x)
         {
             if (x > MaxLen) return 0;
             return x / MaxLen;
         }
+
         static Camera cam;
+
         private static Point2 CamPosit
         {
             set
             {
                 camPos = value;
                 cam.Position = value;
-                cam.Render(rays, MaxLen);
+                cam.Render(RAYS, MaxLen);
             }
             get => camPos;
         }
 
-        static Point2 camPos = new Point2(275, 160);
+        static Point2 camPos = new Point2(50, 50);
 
         static void Main(string[] args)
         {
-            window = new RenderWindow(new VideoMode(1980, 1080), "Window", Styles.Default);
-            window.Closed += (o, args) => { window.Close(); };
-            
-            window.SetActive(true);
-            window.KeyPressed += KeyHandler;
+            _window = new RenderWindow(new VideoMode((uint) Wight, (uint) Height), "Window", Styles.Default);
+            _window.Closed += (o, args) => { _window.Close(); };
+
+            _window.SetActive(true);
+            _window.KeyPressed += KeyHandler;
             Grid mapGrid = new Grid(@"testMap.png", Color.White);
 
-            Grid grid = new Grid(1980, 1080, Color.White);
-            Rectangle rect = new Rectangle(new Point2(0, 1080 / 2), 1980, 1080 / 2, grid);
+            Grid grid = new Grid((uint) Wight, (uint) Height, Color.White);
+            Rectangle rect = new Rectangle(new Point2(0, Height / 2), Wight, Height / 2, grid);
             rect.SetRect(Color.Green, 0);
-            cam = new Camera(camPos, angle, 100, RenderMode.SingleRender) { grid = mapGrid };
+            cam = new Camera(camPos, _angle, 100, RenderMode.SingleRender) {grid = mapGrid};
             Stopwatch time = new Stopwatch();
-            cam.Render(rays, MaxLen);
-            while (window.IsOpen)
+            cam.Render(RAYS, MaxLen);
+            while (_window.IsOpen)
             {
                 time.Start();
                 grid.ClearLayer(1);
-                (window as Window).DispatchEvents();
+                (_window as Window).DispatchEvents();
 
                 var copy = new List<Point2>();
                 lock (Menenger.Buffer)
                     copy.AddRange(from local in Menenger.Buffer
-                                  let p = local as Point2
-                                  select p);
+                        let p = local as Point2
+                        select p);
                 foreach (Point2 epoint in copy)
                 {
-                    var H = (1080 / 2 - PerspectiveHeight(epoint.Len) * 1080 / 2);
-                    Point2 posit = new Point2(Convert.ToInt32(epoint.tag) * 1980 / rays, 1080 / 2);
-                    Rectangle imagePart = new Rectangle(posit, GetEven((double)1980 / rays), H, grid);
-                    imagePart.SetFromCenter(posit.ChangedFor(1980 * 2 / rays , 0).Rounded(), epoint.Color);
+                    var H = (Height / 2 - PerspectiveHeight(epoint.Len) * Height / 2);
+                    Point2 posit = new Point2(Convert.ToInt32(epoint.tag) * Wight / RAYS, Height / 2);
+                    Rectangle imagePart = new Rectangle(posit, GetEven((double) Wight / RAYS), H, grid);
+                    imagePart.SetFromCenter(posit.ChangedFor(Wight * 2 / RAYS, 0).Rounded(), epoint.Color);
                 }
-                grid.ShowToScreen(window);
-                window.Display();
-                window.SetTitle($"FPS {1000 / (double)(time.ElapsedMilliseconds)}");
+
+                grid.ShowToScreen(_window);
+                _window.Display();
+                _window.SetTitle($"FPS {1000 / (double) (time.ElapsedMilliseconds)}");
                 time.Reset();
             }
         }
@@ -120,30 +127,29 @@ namespace SelfGraphics
         {
             if (e.Code == Keyboard.Key.W)
             {
-                CamPosit = CamPosit.ChangedFor(0, 50);
+                CamPosit = CamPosit.ChangedFor(0, 20);
             }
             else if (e.Code == Keyboard.Key.S)
             {
-                CamPosit = CamPosit.ChangedFor(0, -50);
+                CamPosit = CamPosit.ChangedFor(0, -20);
             }
+
             if (e.Code == Keyboard.Key.A)
             {
-                CamPosit = CamPosit.ChangedFor(-50, 0);
+                CamPosit = CamPosit.ChangedFor(-20, 0);
             }
             else if (e.Code == Keyboard.Key.D)
             {
-                CamPosit = CamPosit.ChangedFor(50, 0);
-            }else if(e.Code == Keyboard.Key.Q)
+                CamPosit = CamPosit.ChangedFor(20, 0);
+            }
+            else if (e.Code == Keyboard.Key.Q)
             {
                 CamAngel = CamAngel + 15;
             }
-            else if(e.Code == Keyboard.Key.E)
+            else if (e.Code == Keyboard.Key.E)
             {
                 CamAngel = CamAngel - 15;
             }
-
         }
     }
-
 }
-
